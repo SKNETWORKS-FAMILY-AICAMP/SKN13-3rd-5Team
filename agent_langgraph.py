5c5fd69cdb69b4ac80743d4ea0aab036d9de0bb9
Gogimin <kzm0502@naver.com>
2025-06-27 15:33:49 +0900

    feat: [김지민] temp image upload
    temp image upload

--- agent_langgraph.py
+++ agent_langgraph.py
@@ -13,7 +13,7 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.runnables import *
 from llm_tools.retriever import RAG_tool
 from llm_tools.get_weather import get_weather_by_location_and_date
-from chat_history_manager import ChatHistoryManager
+from chat_history_manager2 import ChatHistoryManager
 
 load_dotenv()
 
